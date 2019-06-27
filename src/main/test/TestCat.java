ed7ebe13294126ded129fa0484bb505d90f426d3
triumphant-xu <736966314@qq.com>
2019-06-27 08:51:07 +0800

    依赖注入测试，bean注入与@component注入

--- src/main/test/TestCat.java
+++ src/main/test/TestCat.java
@@ -1,3 +1,4 @@
+import com.controller.CatController;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,5 +11,12 @@ public class TestCat {
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
         Cat cat=(Cat)context.getBean("cat");
         cat.sing();
+    }
+    @Test
+    public  void test2(){
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        CatController catController=(CatController) context.getBean("catController");
+        catController.showCat();
+
     }
 }
